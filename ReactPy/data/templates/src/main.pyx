d9e05eb0d3ef86616921105b20f8ba3444f76746
RudreshVeerkhare <veerkharerudresh@gmail.com>
2022-03-25 22:05:42 +0530

    Added initial template similar to reactjs

--- ReactPy/data/templates/src/main.pyx
+++ ReactPy/data/templates/src/main.pyx
@@ -1,67 +1,27 @@
 import ReactPy
 from browser import document
-from browser.timer import set_interval, clear_interval
-
-def Timer(props):
-    seconds, setSeconds = ReactPy.useState(0)
-
-
-    def _tick():
-        setSeconds(lambda s: s + 1)
-
-    def __effect():
-        __interval = set_interval(lambda: _tick(), 1000)
-        return lambda: clear_interval(__interval)
-
-    ReactPy.useEffect(__effect, [])
-
-    return <div>Seconds : {seconds}</div>
-
-
-
-def LiveInput(props):
-    value, setValue = ReactPy.useState("")
-
-    def __effect():
-        print("Input Effect Called!")
-        return lambda: print("Input Effect Cancelled!") 
-    
-    ReactPy.useEffect(__effect, [value])
-
-    return  <div>
-                <input value={value} onInput={lambda e: setValue(e.target.value)}/>
-                <p>{value}</p>
-            </div>
-
-def Counter(props):
-    count, setCount = ReactPy.useState(1)
-
-    def __effect():
-        print("Effect Called!")
-
-    ReactPy.useEffect(__effect, [count])
-
-    return  <h1 
-                onClick={lambda e: setCount(lambda c: c + 1)} 
-                className="hello-world" 
-                style={{
-                    "userSelect": "none",
-                    "color": "red"
-                }}
-            >
-               Count :{count}     
-            </h1>
-
+import "index.css"
+import "app.css"
 
 def App(props):
-    visible, setVisible = ReactPy.useState(True)
 
-    return  <div>
-                <LiveInput/>
-                <Timer/>
-                <button onClick={lambda e: setVisible(True)}>Show</button>
-                <button onClick={lambda e: setVisible(False)}>Hide</button>
-                {<Counter/> if visible else <span/>}
+    return  <div className="App">
+                <header className="App-header">
+                    <div className="frame">
+                        <img src="react-rings-gradient.svg" className="App-logo" alt="logo" />
+                    </div>
+                        <p>
+                            Edit <code>{" src/main.pyx "}</code> and save to reload.
+                        </p>
+                    <a
+                        className="App-link"
+                        href="https://github.com/RudreshVeerkhare/ReactPy"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Explore ReactPy
+                    </a>
+                </header>
             </div>
 
 
